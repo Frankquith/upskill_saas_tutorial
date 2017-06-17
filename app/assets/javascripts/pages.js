17cf8aaabfe66509d761115ac3e7b075ed3b300c
Michael Asquith <michael.asquith@me.com>
2017-06-17 04:23:11 +0000

    Added profiles create action, re-enabled fading flash messages

--- app/assets/javascripts/pages.js
+++ app/assets/javascripts/pages.js
@@ -1,5 +1,5 @@
-// // Fade out flash messages after a delay of one second.
+// Fade out flash messages after a delay of one second, for ten seconds.
 
-// $(document).on('turbolinks:load', function(){
-//   $('.alert').delay(1000).fadeOut(4000);
-// });
+$(document).on('turbolinks:load', function(){
+  $('.alert').delay(1000).fadeOut(10000);
+});
