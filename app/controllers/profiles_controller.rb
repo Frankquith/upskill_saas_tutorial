fab75a9400e1d78d03ce87e217574719078bf102
Michael Asquith <michael.asquith@me.com>
2017-06-17 03:38:55 +0000

    Added user profiles form

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -3,5 +3,15 @@ class ProfilesController < ApplicationController
   # GET to /users/:user_id/profile/new
   def new
     # Render blank profile details form
+    @profile_form = Profile.new
   end
+  
+  def create
+    @profile = Profile.new(profile_params)
+  end
+  
+  private
+    def profile_params
+      params.require(:profile).permit()
+    end
 end
